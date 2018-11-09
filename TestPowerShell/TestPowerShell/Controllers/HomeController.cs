8836b963d95f67240b4767be231644f9356a25ee
Shawn <shawn@miniasp.com>
2018-11-09 10:25:41 +0800

    first commit

---
+++ TestPowerShell/TestPowerShell/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Web.Mvc;
+
+namespace TestPowerShell.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            WSManConnectionInfo connectionInfo = new WSManConnectionInfo();
+            connectionInfo.ComputerName = "";
+
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.Runspace = runspace;
+                ps.AddScript("Get-Process");
+                var results = ps.Invoke();
+                // Do something with result ... 
+                foreach (var result in results)
+                {
+                    ViewBag.Message += result.Members["Id"].Value + " ";
+                    ViewBag.Message += result.Members["ProcessName"].Value + " ";
+                    ViewBag.Message += result.Members["PrivateMemorySize64"].Value + "<br>";
+                }
+            }
+            runspace.Close();
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
